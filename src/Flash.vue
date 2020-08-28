04dbd3dc8d6276f986e64a247dce768ea8cdacdf
ch89 <chhq89@gmail.com>
2020-08-28 23:06:42 +0200

    add more

--- src/Flash.vue
+++ src/Flash.vue
@@ -1,3 +1,33 @@
 <template>
-	<div>Flash component</div>
+	<div class="flash">
+		<div v-for="notification in notifications" class="alert" :class="`alert-${notification.type}`" v-text="notification.message"></div>
+	</div>
 </template>
+
+<script>
+	import bus from "./bus"
+
+	export default {
+		data() {
+			return {
+				notifications: []
+			}
+		},
+		created() {
+			bus.$on("flash", notification => {
+				this.notifications.push(notification)
+
+				setTimeout(() => this.notifications.splice(this.notifications.indexOf(notification), 1), 3000)
+			})
+		}
+	}
+</script>
+
+<style scoped>
+	.flash {
+		display: fixed;
+		bottom: 2rem;
+		right: 2rem;
+		z-index: 100;
+	}
+</style>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import Flash from "./Flash"
+import bus from "./bus"
 
 export default {
 	install(Vue) {
 		Vue.component("flash", Flash)
+
+		Vue.prototype.$flash = {
+			notification(message, type) {
+				bus.$emit("flash", { message, type })
+			},
+			success(message) {
+				this.notification(message, "success")
+			},
+			danger(message) {
+				this.notification(message, "danger")
+			}
+		}
 	}
 }
